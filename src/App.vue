bd3fb1bf4082c90a8b54081d3f3ffd59d2978c42
wanghongwei <845487715@qq.com>
2017-11-23 17:37:52 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+	<router-link to="/view1">Go to view1</router-link>
+	<router-link to="/view2">Go to view2</router-link>
+	<router-view></router-view>
+  </div>
+</template>
+
+<script>
+import hello from '@/components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
